615153b6b2cace226352f20f37e7aac953c81f47
Mateusz <bloxorzykigi@gmail.com>
2021-11-15 12:55:54 +0100

    refactor: formating

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,85 +1,92 @@
 import React from 'react';
-import {Box} from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
-// import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
-import { useHistory } from "react-router-dom";
+import { Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom';
 
-export const HomePage = () => {
-    const classes = useStyles();
-    let history = useHistory();
+export const HomePage = function () {
+  const classes = useStyles();
+  const history = useHistory();
 
-    function leftClick() {
-        history.push("/how");
-    }
-    function rightClick() {
-        history.push("/videos");
-    }
-    return (
-        <div className={classes.wrapper}>
-            <Box className={classes.leftBox} onClick={leftClick}>
-                <h1>
-                    Welcome stranger!
-                </h1>
-                <p>
-                    Has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-                    galley of type and scrambled it to make a type specimen book. It has survived not only five
-                    centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was
-                    popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
-                    more recently with desktop publishing software like Aldus PageMaker including versions of Lorem
-                    Ipsum.
-                </p>
-            </Box>
-            <Box className={classes.rightBox} onClick={rightClick}>
-                <h1>
-                    Videos
-                </h1>
-                <p>
-                    If you already know how VideoSpace works check out videos we are offering. Hope you will find something interesting.
-                </p>
-                {/*<VideoLibraryIcon/>*/}
-            </Box>
-        </div>
-    );
+  function leftClick() {
+    history.push('/how');
+  }
+
+  function rightClick() {
+    history.push('/videos');
+  }
+
+  return (
+    <div className={classes.wrapper}>
+      <Box className={classes.leftBox} onClick={leftClick}>
+        <h1>
+          Welcome stranger!
+        </h1>
+        <p>
+          Has been the industry standard dummy text ever since the 1500s, when an unknown printer
+          took a
+          galley of type and scrambled it to make a type specimen book. It has survived not only
+          five
+          centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          It was
+          popularised in the 1960s with the release of Least sheets containing Lorem Ipsum
+          passages, and
+          more recently with desktop publishing software like Aldus PageMaker including versions of
+          Lorem
+          Ipsum.
+        </p>
+      </Box>
+      <Box className={classes.rightBox} onClick={rightClick}>
+        <h1>
+          Videos
+        </h1>
+        <p>
+          If you already know how VideoSpace works check out videos we are offering. Hope you will
+          find something interesting.
+        </p>
+        {/* <VideoLibraryIcon/> */}
+      </Box>
+      ;
+    </div>
+  );
 };
 
 const useStyles = makeStyles({
-    wrapper: {
-        height: 'calc(100vh - 130px)',
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    leftBox: {
-        padding: '20px',
-        backgroundColor: '#FF9472',
-        width: '400px',
-        height: '400px',
-        border: "1px solid white",
-        wordWrap: 'break-word',
-        animation: `$rightEffect 500ms ease-out`,
-        boxShadow: 'rgb(0 0 0 / 50%) 2px 2px 30px 1px',
-        cursor: "pointer",
-        marginRight: '60px',
-    },
-    rightBox: {
-        padding: '20px',
-        backgroundColor: '#FF9472',
-        width: '400px',
-        height: '400px',
-        border: "1px solid white",
-        wordWrap: 'break-word',
-        animation: `$rightEffect 500ms ease-out`,
-        boxShadow: 'rgb(0 0 0 / 50%) 2px 2px 30px 1px',
-        cursor: "pointer",
-       
-    },
-    "@keyframes rightEffect": {
-        "0%": {
-            opacity: 0,
-        },
-        "100%": {
-            opacity: 1,
-        }
-    },
-}, {name: 'HomePage'});
+  wrapper: {
+    height: 'calc(100vh - 130px)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  leftBox: {
+    padding: '20px',
+    backgroundColor: '#FF9472',
+    width: '400px',
+    height: '400px',
+    border: '1px solid white',
+    wordWrap: 'break-word',
+    animation: '$rightEffect 500ms ease-out',
+    boxShadow: 'rgb(0 0 0 / 50%) 2px 2px 30px 1px',
+    cursor: 'pointer',
+    marginRight: '60px',
+  },
+  rightBox: {
+    padding: '20px',
+    backgroundColor: '#FF9472',
+    width: '400px',
+    height: '400px',
+    border: '1px solid white',
+    wordWrap: 'break-word',
+    animation: '$rightEffect 500ms ease-out',
+    boxShadow: 'rgb(0 0 0 / 50%) 2px 2px 30px 1px',
+    cursor: 'pointer',
 
+  },
+  '@keyframes rightEffect': {
+    '0%': {
+      opacity: 0,
+    },
+    '100%': {
+      opacity: 1,
+    },
+  },
+}, { name: 'HomePage' });
--- client/src/pages/VideoPage.js
+++ client/src/pages/VideoPage.js
@@ -1,228 +1,272 @@
-import React, {useEffect, useRef, useState} from 'react';
-import Container from "@material-ui/core/Container";
-import {Box, Button, Typography} from "@material-ui/core";
-import {makeStyles} from "@material-ui/core/styles";
-import * as faceApi from "face-api.js";
+import React, { useEffect, useRef, useState } from 'react';
+import Container from '@material-ui/core/Container';
+import { Box, Button, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import * as faceApi from 'face-api.js';
 
 const expressionMap = {
-    neutral: " neutral 😶",
-    happy: " happy 😄",
-    sad: "sad 😞",
-    angry: "angry 🤬",
-    fearful: "fearful 😖",
-    disgusted: "disgusted 🤢",
-    surprised: "surprised 😲"
+  neutral: ' neutral 😶',
+  happy: ' happy 😄',
+  sad: 'sad 😞',
+  angry: 'angry 🤬',
+  fearful: 'fearful 😖',
+  disgusted: 'disgusted 🤢',
+  surprised: 'surprised 😲',
 };
 
-export const VideoPage = (props) => {
-    const classes = useStyles();
-    const videoRef = useRef()
-    const movieRef = useRef()
-    const [emotion, setEmotion] = useState()
-    const [timestamps, setTimestamps] = useState([])
-    const [now, setNow] = useState(false);
-    const [info, setInfo] = useState(false);
+export const VideoPage = () => {
+  const classes = useStyles();
+  const videoRef = useRef();
+  const movieRef = useRef();
+  const [emotion, setEmotion] = useState();
+  const [timestamps, setTimestamps] = useState([]);
+  const [now, setNow] = useState(false);
+  const [info, setInfo] = useState(false);
+  const stream = useRef(false)
 
-    useEffect(() => {
-        if (now === true) {
-            async function fetchModel() {
-                await faceApi.nets.tinyFaceDetector.load('/models/')
-                await faceApi.loadFaceExpressionModel(`/models/`);
-                videoRef.current.srcObject = await navigator.mediaDevices.getUserMedia({
-                    video: {facingMode: "user"}
-                });
-            }
-
-            try {
-                fetchModel()
-            } catch (e) {
-                console.log('somthing wrong');
-            }
-        }
-    }, [now])
-
-    useEffect(() => {
-        if (emotion === 'happy') {
-            setTimestamps(prev => [...new Set([...prev, movieRef.current.currentTime])])
-        }
-    }, [emotion]);
-
-
-    const onPlay = async () => {
-        // console.log('look out')
-        if (!faceApi.nets.tinyFaceDetector.params) {
-            return
-        }
-        const options = new faceApi.TinyFaceDetectorOptions({
-            inputSize: 512,
-            scoreThreshold: 0.5
+  useEffect(() => {
+    if (now === true) {
+      async function fetchModel() {
+        await faceApi.nets.tinyFaceDetector.load('/models/');
+        await faceApi.loadFaceExpressionModel('/models/');
+          stream.current = await navigator.mediaDevices.getUserMedia({
+          video: { facingMode: 'user' },
         });
-        const result = await faceApi
-            .detectSingleFace(videoRef.current, options)
-            .withFaceExpressions()
+        videoRef.current.srcObject = stream.current
+      }
 
-        if (result) {
-            const arr = Object.entries(result.expressions);
-            const max = arr.reduce(
-                (acc,
-                 current) => {
-                    return acc[1] > current[1] ? acc : current
-                }, []
-            )
-            setEmotion(max[0])
-        }
-
+      try {
+        fetchModel();
+      } catch (e) {
+        console.log('somthing wrong');
+      }
     }
+  }, [now]);
 
-    useEffect(() => {
-        const time = setInterval(() => onPlay(), 300)
-        return () => {
-            clearInterval(time)
-        }
-    }, [])
-
-    const seekTo = (time) => {
-        movieRef.current.currentTime = time;
+  useEffect(() => {
+    if (emotion === 'happy') {
+      setTimestamps((prev) => [...new Set([...prev, movieRef.current.currentTime])]);
     }
+  }, [emotion]);
 
-    return (
-        <Container className={classes.wrapper}>
-            {now ?
-                <Box className={classes.camera}>
-                    <video
-                        ref={videoRef}
-                        style={{width: '360px', border: "1px solid white",}}
-                        autoPlay
-                        muted
-                        onPlay={onPlay}
-                    />
-                    <Typography variant="h4">
-                        {expressionMap[emotion]}
-                    </Typography>
-                    <Box className={classes.stampsWrapper}>
-                        {timestamps.map(stamp =>
-                            <Box className={classes.timestampBox} onClick={() => {
-                                seekTo(stamp)
-                            }}>
-                                <Typography variant="h6">
-                                    {stamp}
-                                </Typography>
-                            </Box>
-                        )}
-                    </Box>
+  useEffect(() => {
+    let time;
+    if (now) {
+      time = setInterval(() => onPlay(), 300);
+    }
+    return () => {
+      clearInterval(time);
+    };
+  }, [now]);
+
+  const stop = () => {
+    stream.current.getTracks().forEach(function(track) {
+      track.stop();
+    });
+
+    videoRef.current.srcObject = null;
+    setNow(false)
+  }
+
+  const seekTo = (time) => {
+    movieRef.current.currentTime = time;
+  };
+
+  const onPlay = async () => {
+    if (!faceApi.nets.tinyFaceDetector.params) {
+      return;
+    }
+    const options = new faceApi.TinyFaceDetectorOptions({
+      inputSize: 512,
+      scoreThreshold: 0.5,
+    });
+    const result = await faceApi
+      .detectSingleFace(videoRef.current, options)
+      .withFaceExpressions();
+
+    if (result) {
+      const arr = Object.entries(result.expressions);
+      const max = arr.reduce((
+        acc,
+        current,
+      ) => (acc[1] > current[1] ? acc : current), []);
+      setEmotion(max[0]);
+    }
+  };
+
+  return (
+    <Container className={classes.wrapper}>
+      {now
+        ? (
+          <Box className={classes.camera}>
+            <Button onClick={stop}> Close </Button>
+            <video
+              ref={videoRef}
+              style={{
+                width: '360px',
+                border: '1px solid white',
+              }}
+              autoPlay
+              muted
+              onPlay={onPlay}
+            />
+            <Typography variant="h4">
+              {expressionMap[emotion]}
+            </Typography>
+            <Box className={classes.stampsWrapper}>
+              {timestamps.map((stamp) => (
+                <Box
+                  key={stamp}
+                  className={classes.timestampBox}
+                  onClick={() => {
+                    seekTo(stamp);
+                  }}
+                >
+                  <Typography variant="h6">
+                    {stamp}
+                  </Typography>
                 </Box>
-                :
-                <Button onClick={() => {
-                    setNow(true)
-                }} style={{color: 'white'}}>Open</Button>
-            }
-            <Box className={classes.playerWrapper}>
-                <video className={classes.video} controls crossOrigin="anonymous" ref={movieRef}>
-                    <source src={`http://localhost:4000/video/${2}`} type="video/mp4"/>
-                    <track label="English" kind="captions" srcLang="en" src={`http://localhost:4000/video/${2}/caption`}
-                           default/>
-                </video>
+              ))}
             </Box>
-            {info ?
-                <Box className={classes.content}>
-                    <h1>
-                        Welcome stranger!
-                    </h1>
-                    <p>
-                        Has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-                        galley of type and scrambled it to make a type specimen book. It has survived not only five
-                        centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It
-                        was
-                    </p>
-                </Box> :
-                <Button onClick={() => {
-                    setInfo(true)
-                }} style={{color: 'white'}}>Open</Button>
-            }
-        </Container>
-    );
+          </Box>
+        )
+        : (
+          <Button
+            onClick={() => setNow(true)}
+            className={classes.leftOpen}
+            variant="outlined"
+          >
+            Open Camera
+          </Button>
+        )}
+      <Box className={classes.playerWrapper}>
+        <video className={classes.video} controls crossOrigin="anonymous" ref={movieRef}>
+          <source src={`http://localhost:4000/video/${2}`} type="video/mp4"/>
+          <track
+            label="English"
+            kind="captions"
+            srcLang="en"
+            src={`http://localhost:4000/video/${2}/caption`}
+            default
+          />
+        </video>
+      </Box>
+      {info
+        ? (
+          <Box className={classes.content}>
+            <h1>
+              Welcome stranger!
+            </h1>
+            <p>
+              Has been the industry standard dummy text ever since the 1500s, when an unknown
+              printer took a
+              galley of type and scrambled it to make a type specimen book. It has survived not only
+              five
+              centuries, but also the leap into electronic typesetting, remaining essentially
+              unchanged. It
+              was
+            </p>
+          </Box>
+        )
+        : (
+          <Button
+            onClick={() => {
+              setInfo(true);
+            }}
+            className={classes.rightOpen}
+            variant="outlined"
+          >
+            Info
+          </Button>
+        )}
+    </Container>
+  );
 };
-
 
 const useStyles = makeStyles({
-    wrapper: {
-        height: 'calc(100vh - 130px)',
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        animation: `$rightEffect 500ms ease-out`,
-        gap: '60px',
-        overflow: 'hidden',
+  wrapper: {
+    height: 'calc(100vh - 130px)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    animation: '$rightEffect 500ms ease-out',
+    gap: '60px',
+    overflow: 'hidden',
+  },
+  playerWrapper: {
+    backgroundColor: 'transparent',
+    height: '600px',
+    maxWidth: '800px',
+    boxShadow: 'rgb(0 0 0 / 50%) 2px 2px 30px 1px',
+  },
+  video: {
+    border: '1px solid white',
+    height: '602px',
+    maxWidth: '800px',
+    backgroundColor: 'black',
+  },
+  content: {
+    color: 'white',
+    padding: '20px',
+    backgroundColor: '#FF9472',
+    width: '400px',
+    height: '600px',
+    border: '1px solid white',
+    wordWrap: 'break-word',
+    backgroundImage: 'url(https://terrigen-cdn-dev.marvel.com/content/prod/1x/online_9.jpg)',
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'scroll',
+    backgroundSize: 'cover',
+    boxShadow: 'rgb(0 0 0 / 50%) 2px 2px 30px 1px',
+    position: 'absolute',
+    right: '-400px',
+    animation: '$leftEffect 0.5s forwards',
+  },
+  camera: {
+    padding: '20px',
+    backgroundColor: '#FF9472',
+    minWidth: '400px',
+    height: '600px',
+    border: '1px solid white',
+    boxShadow: 'rgb(0 0 0 / 50%) 2px 2px 30px 1px',
+    display: 'flex',
+    flexDirection: 'column',
+    position: 'absolute',
+    left: '-450px',
+    animation: '$rightEffect 0.5s forwards',
+  },
+  '@keyframes rightEffect': {
+    '100%': {
+      left: 40,
     },
-    playerWrapper: {
-        backgroundColor: 'transparent',
-        height: '600px',
-        maxWidth: '800px',
-        boxShadow: 'rgb(0 0 0 / 50%) 2px 2px 30px 1px',
+  },
+  '@keyframes leftEffect': {
+    '100%': {
+      right: 40,
     },
-    video: {
-        border: "1px solid white",
-        height: '602px',
-        maxWidth: '800px',
-        backgroundColor: 'black'
-    },
-    content: {
-        color: 'white',
-        padding: '20px',
-        backgroundColor: '#FF9472',
-        width: '400px',
-        height: '600px',
-        border: "1px solid white",
-        wordWrap: 'break-word',
-        backgroundImage: 'url(https://terrigen-cdn-dev.marvel.com/content/prod/1x/online_9.jpg)',
-        backgroundRepeat: 'no-repeat',
-        backgroundAttachment: 'scroll',
-        backgroundSize: 'cover',
-        boxShadow: 'rgb(0 0 0 / 50%) 2px 2px 30px 1px',
-        position: 'absolute',
-        right: '-400px',
-        animation: '$leftEffect 0.5s forwards',
-        animationDelay: '1s',
-    },
-    camera: {
-        padding: '20px',
-        backgroundColor: '#FF9472',
-        minWidth: '400px',
-        height: '600px',
-        border: "1px solid white",
-        boxShadow: 'rgb(0 0 0 / 50%) 2px 2px 30px 1px',
-        display: 'flex',
-        flexDirection: 'column',
-        position: 'absolute',
-        left: '-450px',
-        animation: '$rightEffect 0.5s forwards',
-        animationDelay: '2s',
-    },
-    "@keyframes rightEffect": {
-        "100%": {
-            left: 0
-        }
-    },
-    "@keyframes leftEffect": {
-        "100%": {
-            right: 0
-        }
-    },
-    stampsWrapper: {
-        overflow: 'auto',
-        display: 'flex',
-        flexDirection: ' column',
-        paddingRight: '15px',
-    },
-    timestampBox: {
-        backgroundColor: '#3b27ba',
-        color: 'white',
-        borderRadius: '3px',
-        border: '0.5px solid white',
-        marginTop: '6px',
-        padding: '4px',
-    }
-}, {name: 'HowPage'});
-
-
-
+  },
+  stampsWrapper: {
+    overflow: 'auto',
+    display: 'flex',
+    flexDirection: ' column',
+    paddingRight: '15px',
+  },
+  timestampBox: {
+    backgroundColor: '#3b27ba',
+    color: 'white',
+    borderRadius: '3px',
+    border: '0.5px solid white',
+    marginTop: '6px',
+    padding: '4px',
+  },
+  leftOpen: {
+    color: 'white',
+    position: 'absolute',
+    left: 40,
+  },
+  rightOpen: {
+    color: 'white',
+    position: 'absolute',
+    right: 40,
+  },
+}, { name: 'HowPage' });
